67af41d7daec2c582ddc96539e4a7e321b347a83
Jvaz0715 <James.Vazquez@codeimmersives.com>
2021-03-01 18:54:00 -0500

    Solves Challenge 4

--- main.js
+++ main.js
@@ -46,7 +46,12 @@ for (let i = 0; i < positiveNumbers.length; i++) {
 // Challenge 4
 // Console.log out the even numbers in the following array, INCLUDING the negative ones. There should be four total printings.
 const mixedSignNumbers = [3, 15, 14, -2, -3, -8, -103, 4];
-
+console.log('===Challenge 4===')
+for (let i = 0; i < mixedSignNumbers.length; i++) {
+  if (mixedSignNumbers[i] % 2 == 0) {
+    console.log(mixedSignNumbers[i]);
+  }
+}
 
 // Challenge 5
 // Remove two values from the beginning and one value from the end of the following array, simply by deleting them in the following line. Then console.log out each value individually.
