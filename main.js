214be36c977f3725b6b507f0b4bba0aa79de3697
Jvaz0715 <James.Vazquez@codeimmersives.com>
2021-03-01 18:35:20 -0500

    Solves challenge 2

--- main.js
+++ main.js
@@ -27,6 +27,10 @@ for (let i = 0; i < students.length; i++) {
 // Loop through the following array BACKWARDS, console.logging out each value. 64 should be printed first and 100 last.
 const grades = [100, 80, 110, 75, 83, 64];
 
+for (let i = grades.length - 1; i >=0; i--) {
+  console.log(grades[i])
+};
+
 
 // Challenge 3
 // Console.log out only the even numbers in the following array.
