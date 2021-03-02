fe6262cca2697fe502119ed797932397e6e4e522
Jvaz0715 <James.Vazquez@codeimmersives.com>
2021-03-01 19:50:20 -0500

    Solves Challenge 9

--- main.js
+++ main.js
@@ -17,6 +17,10 @@ const students = [
   'Timothy',
   'Patrick',
   'Steve',
+  'Jimothy',
+  'Pat',
+  'Arnold',
+  'Andy',
 ];
 console.log('===Challenge 1===')
 for (let i = 0; i < students.length; i++) {
@@ -103,11 +107,12 @@ for (let i = 0; i < emptyArray.length; i++) {
   console.log(emptyArray[i])
 };
 
-
-
 // Challenge 9
 // Loop through the `students` array from Challenge 1, starting at index 3 and ending at index 10 (INCLUSIVE), logging out each value individually.
-
+console.log('===Challenge 9===')
+for (let i = 3; i <= 10; i++) {
+  console.log(students[i])
+};
 
 // Challenge 10
 // Loop through the `students` array from Challenge 1, making a COPY of the array, starting at index 3 and ending at index 10 (INCLUSIVE). This is very similar to string building; create a new array, loop through the old one, and put values from the original in the new one. Then log out each value of the copy indivudally.
